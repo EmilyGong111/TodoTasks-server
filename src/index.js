08a64c2d7e213811857ceec8d9df449f1bfa6dff
Emily <mei.gong@icloud.com>
2022-05-26 14:09:44 +1000

    feat: add morgan dotenv helmet and winston

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
+require('dotenv').config();//Read .env file, must have this sentence at top of this file
 const express = require("express");
 const cors = require('cors');
+const helmet = require('helmet')
 const v1Router = require("./routes");
+const logger = require('./utils/logger');
+const morgan = require("morgan");
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(helmet());//Protect server from attack
 app.use(cors({origin:"*"}));
+app.use(morgan('common'));//HTTP request logger middleware for node.js. Will log the request ip, time, and method in terminal. Can only print, but can not store.
 app.use(express.json());
 
 //GET/v1/tasks
 // app.use('/v1')
 //GET/tasks
 app.use(v1Router);
-app.listen(3000,()=>{console.log(`server is listening on 3000`)});
+app.listen(PORT,()=>{logger.info(`server is listening on port: ${PORT}`)}); //Will log the message in the format in logger.js
+
+//monitoring (You many need monitoring in your sever to make sure your sever is working)
+// /health_check
+// /healthz
+
+//sometimes we use morgan with winston to store the requests' info into somewhere. 
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,6 @@ const taskRouter = require('./task');
 
 const v1Router = express.Router();
 
-v1Router.use('/tasks', taskRouter);
+v1Router.use('/tasks', taskRouter);// any path start with /tasks can be access ->.use | only the path is /tasks can be access -> .get
 
 module.exports = v1Router;
